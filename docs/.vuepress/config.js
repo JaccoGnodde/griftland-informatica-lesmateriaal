e276fb7befc0a94a9125ad8319166b42aeea1969
Thijs den Hartog <Thijs@thijsdh.nl>
2019-04-16 15:35:28 +0200

    Updated vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,6 @@ module.exports = {
 	title: 'Griftland Informatica',
 	description: 'Lesmateriaal tbv informatica voortgezet onderwijs',
 	themeConfig: {
-		sidebar: {
-			'/testlesmodule/': ['inleiding', 'test1', 'test2', 'test3']
-		}
+		sidebar: require('../sidebar.json')
 	}
 };
