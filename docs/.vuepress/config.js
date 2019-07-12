edf1e6e0d5f0638be86c67da57761a2d0573862d
Thijs den Hartog <Thijs@thijsdh.nl>
2019-07-12 15:03:13 +0200

    Open secties van een pagina door op het kopje te klikken

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
 	title: 'Griftland Informatica',
 	description: 'Lesmateriaal tbv informatica voortgezet onderwijs',
 	themeConfig: {
+		activeHeaderLinks: false,
 		sidebar: require('../sidebar.json')
 	}
 };
